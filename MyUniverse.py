571a3e63a932abb5c012c03a3e4c918ce1ff7a51
Charles Reid <charlesreid1@gmail.com>
2022-12-31 12:50:35 -0800

    add example apps/driver

--- MyUniverse.py
+++ MyUniverse.py
@@ -2,9 +2,10 @@
 
 import wx
 import sys
-from src import app
+#from src import app
+from src import app2
 import inspect
 
 if __name__ == "__main__":
-	app.main(sys.argv)
-	#test(sys.argv)
+	#app.main(sys.argv)
+	app2.main()
